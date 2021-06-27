2c30155e13031f5cc25197b4e3611f97b510f994
Dmitriy <dimataranapa12@gmail.com>
2021-06-27 13:43:23 +0300

    upd

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple-task-manager
+task manager
---
+++ SimpleTaskManager/Controllers/TasksController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTaskManager.Controllers
+{
+    [ApiController]
+    [Route("api/tasks")]
+    public class TasksController : ControllerBase
+    {
+        private readonly ILogger<TasksController> _logger;
+
+        public TasksController(ILogger<TasksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetTasks()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetExpiring()
+        {
+            return Ok();
+        }
+    }
+}
